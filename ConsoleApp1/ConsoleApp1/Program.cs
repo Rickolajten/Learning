3304188b5256840930b98e2c4b290becc2e51f58
Rickolajten <svenstaviken@hotmail.com>
2020-08-24 22:01:41 +0200

    v1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Skapa ett program som skriver ut talen 10, 11, 12 o.s.v. upp till och med 30.
+*  Därefter ska programmet skriva ut talen 200, 199, 198 o.s.v. ner till och med 180.
+*  Slutligen ska programmet skriva ut talen 1000, 1050, 1100, 1150 o.s.v. upp till och med 1400.
+*/
+
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i < 31; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            
+            for (int y = 200; y > 179; y--)
+            {
+                Console.WriteLine(y);
+            }
+            Console.WriteLine();
+           
+            for (int x = 1000; x < 1450; x += 50)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine(); 
+
+        }
+    }
+}
---
+++ Uppgift 4.18/Uppgift 4.18/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 
+ * Skapa ett program där användaren ska skriva in ett heltal.
+ * Efter att hen har gjort det ska programmet fråga om användaren vill skriva in ett heltal till eller inte.
+ * Användaren ska fortsätta att få skriva in nya heltal ända tills hen inte vill göra det mer.
+ * När användaren inte vill skriva in mer tal ska programmet skriva ut vilket som var det största talet som användaren skrev in.
+ */
+
+using System;
+
+namespace Uppgift_4._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            int störst = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vill du skriva in ett tal till? Skriv j eller n");
+            string skrivaIgen = Console.ReadLine();
+            while (skrivaIgen == "j")
+            {
+                Console.WriteLine("Skriv in ett tal: ");
+                int nyttTal = int.Parse(Console.ReadLine());
+
+                if (nyttTal > störst)
+                {
+                    störst = nyttTal;
+                }
+
+                Console.WriteLine($"Det största talet är {störst}");
+            }
+        }
+    }
+}
