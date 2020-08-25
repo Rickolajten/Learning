9c026791e2571094725e22814a0f15b5b04c85c3
Rickolajten <svenstaviken@hotmail.com>
2020-08-25 15:03:56 +0200

    v1

---
+++ Uppgift 4.21/Uppgift 4.21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_4._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string helTal = Console.ReadLine();
+
+            for (int i = 0; i < helTal.Length; i++)
+            {
+                string tecken = helTal[i].ToString();
+                int siffra = int.Parse(tecken);
+
+                if (siffra == 9)
+                {
+                    Console.Write("0");
+                }
+                else
+                {
+                    Console.Write(siffra + 1);
+                }
+            }
+        }
+    }
+}
